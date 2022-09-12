7536874bab815d3da17a9c081dfd6e5c1ae43d84
Bernardo Enock <91747672+bernardoenock@users.noreply.github.com>
2022-09-12 16:51:08 -0300

    fix: deploy

--- motors-shop/src/services/urls.api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const motorShopAPI = axios.create({
-  baseURL: "https://motor-shop.herokuapp.com/",
-});
-
-export const viaCepAPI = axios.create({
-  baseURL: "https://viacep.com.br/",
-});
