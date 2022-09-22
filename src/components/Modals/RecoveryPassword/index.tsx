e8daf4abe0ab2ada50f0bcedb953f49421e45053
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-09-21 22:05:39 -0300

    feat: recovery password modal and providers

--- src/components/Modals/RecoveryPassword/index.tsx
+++ src/components/Modals/RecoveryPassword/index.tsx
@@ -4,15 +4,20 @@ import { useForm } from "react-hook-form";
 
 import GeneralInput from "../../Forms/Components/Inputs/GeneralInput";
 import { ButtonPrimary } from "../../Button";
-import { Form, TopModal } from "./styles";
+import { CodeButton, Form, TopModal } from "./styles";
 
 import { CloseModalBtn } from "../../Button/CloseModalBtn";
+import { useState } from "react";
+import Modal from "..";
+import NewPassword from "./NewPassword";
+import { useUpdatePassword } from "../../../Providers/User/passwordUser";
+import { useLoad } from "../../../Providers/Loading";
 
-interface IHandleModal {
+interface IProps {
   handleModal: () => void;
 }
 
-const RecoveryPassword = ({ handleModal }: IHandleModal) => {
+const RecoveryPassword: React.FC<IProps> = ({ handleModal }) => {
   const schema = yup.object().shape({
     email: yup
       .string()
@@ -28,28 +33,49 @@ const RecoveryPassword = ({ handleModal }: IHandleModal) => {
     resolver: yupResolver(schema),
   });
 
+  const [show, setShow] = useState(false);
+
+  const { showLoad } = useLoad();
+  const { recoveryCode } = useUpdatePassword();
+
   const handleRequest = (data: { email?: string }) => {
-    console.log(data);
+    showLoad();
+    recoveryCode(data.email!);
   };
 
   return (
-    <Form onSubmit={handleSubmit(handleRequest)}>
-      <TopModal>
-        <h1>Recuperação de senha:</h1>
+    <>
+      <Modal show={show} close={() => setShow(false)} height="20rem">
+        <NewPassword
+          handleModal={() => {
+            setShow(false);
+            handleModal();
+          }}
+        />
+      </Modal>
+      <Form onSubmit={handleSubmit(handleRequest)}>
+        <TopModal>
+          <h1>Recuperação de senha:</h1>
 
-        <CloseModalBtn onClick={handleModal} />
-      </TopModal>
+          <CloseModalBtn onClick={handleModal} />
+        </TopModal>
 
-      <GeneralInput
-        label="Digite seu e-mail"
-        register={register}
-        name={"email"}
-        error={errors.email?.message}
-        type="email"
-        placeholder="Digitar e-mail"
-      />
-      <ButtonPrimary type="submit">Enviar</ButtonPrimary>
-    </Form>
+        <GeneralInput
+          label="Digite seu e-mail"
+          register={register}
+          name={"email"}
+          error={errors.email?.message}
+          type="email"
+          placeholder="Digitar e-mail"
+        />
+        <CodeButton>
+          <button onClick={() => setShow(true)} type="button">
+            Já tenho um código
+          </button>
+        </CodeButton>
+        <ButtonPrimary type="submit">Enviar</ButtonPrimary>
+      </Form>
+    </>
   );
 };
 
